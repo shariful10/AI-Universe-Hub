9f04d4455a64c40505487edf1c2fb347df2dbdda
Sk Shariful Islam <skshariful10@gmail.com>
2023-03-03 00:34:35 +0600

    Font Family Added

--- js/index.js
+++ js/index.js
@@ -17,9 +17,9 @@ const showUniverseHub = (data) => {
                 </div>
                 <div class="card-body">
                     <h3 class="card-title font-work font-semibold text-[25px] mb-4">Features</h3>
-                    <p class="font-work">1. ${card.features[0]}</p>
-                    <p class="font-work">2. ${card.features[1]}</p>
-                    <p class="font-work">3. ${card.features[2]}</p>
+                    <p class="font-work text-darker">1. ${card.features[0]}</p>
+                    <p class="font-work text-darker">2. ${card.features[1]}</p>
+                    <p class="font-work text-darker">3. ${card.features[2]}</p>
                 </div>
                 <div class="px-[25px]">
                     <hr>
@@ -29,7 +29,7 @@ const showUniverseHub = (data) => {
                         <h3 class="card-title font-work font-semibold text-[25px] mb-4">${card.name}</h3>
                         <div class="flex  space-x-[8px]">
                             <img class="" src="../images/published_in.png" />
-                            <p> ${card.published_in}</p>
+                            <p class="font-work text-darker">${card.published_in}</p>
                         </div>
                     </div>
                     <div>
